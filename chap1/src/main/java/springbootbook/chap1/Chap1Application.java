203997800877a0ef4872de72a3f39c14a019fcb5
msunilhyd <msunilhyd@gmail.com>
2020-09-24 18:12:40 +0530

    24-Sep-20: Thur :- Added Gradle support

--- chap1/src/main/java/springbootbook/chap1/Chap1Application.java
+++ chap1/src/main/java/springbootbook/chap1/Chap1Application.java
@@ -2,10 +2,17 @@ package springbootbook.chap1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Chap1Application {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Chap1Application.class, args);
 	}
